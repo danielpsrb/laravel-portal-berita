76ef0d24c9872fe20b006301f7cb38248e02aab5
danielpsrb <danielpsrb01@gmail.com>
2024-03-01 13:22:47 +0700

    update nav & homepage

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+    console.log('isUser?', user)
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
@@ -11,21 +12,30 @@ const Navbar = () => {
                     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
                 </div>
                 <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
                     </div>
-                </div>
-                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                    <li>
-                        <a className="justify-between">
-                            Dashboard
-                            <span className="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        {!user ?
+                        <>
+                            <li className='text-black'><a>Login</a></li>
+                            <li className='text-black'><a>Register</a></li>
+                        </>
+                        : 
+                        <>
+                            <li>
+                                <a className="justify-between">
+                                    Dashboard
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </>
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -9,7 +9,7 @@ export default function Homepage(props) {
     return (
         <div className='min-h-screen bg-slate-500 text-white text-2xl'>
             <Head title={props.title} />   
-            <Navbar />         
+            <Navbar user={props.auth.user} />         
             <div className='flex justify-center items-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch gap-4 p-4'>
                 <NewsList news={props.news.data} />
             </div>
