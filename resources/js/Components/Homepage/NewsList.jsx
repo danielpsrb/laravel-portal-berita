3a8e069efd72e97b1eafb4169f9f847679d54252
danielpsrb <danielpsrb01@gmail.com>
2024-03-01 11:20:47 +0700

    update NewList component

--- resources/js/Components/Homepage/NewsList.jsx
+++ resources/js/Components/Homepage/NewsList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const isNews = (news) => {
     return news.map((data, i)=> {
-        return <div key={i} className="card w-96 bg-base-100 shadow-xl">
+        return <div key={i} className="card w-full lg:w-96 bg-base-100 shadow-xl">
         <figure>
             <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
         </figure>
